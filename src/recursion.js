c4ab8be641c123d6dca5afca0f90431c7f2bbc92
Steven Duncan <steven.k.duncan@gmail.com>
2018-09-08 12:09:57 -0500

    factorial and sum tests

--- src/recursion.js
+++ src/recursion.js
@@ -6,12 +6,18 @@
 // denoted by n!, is the product of all positive integers less than or equal to n.
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
-var factorial = function(n) {
+
+//base case: n = 0; reduction: n * n-1 
+var factorial = (n) => { 
+	return n < 0 ? null : n === 0 ? 1 : n * factorial(n - 1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
-var sum = function(array) {
+ 
+//basecase: array length
+var sum = (array) => {
+    return (array.length === 0) ? 0 : array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
