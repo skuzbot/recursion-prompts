938d8a3a46832c5d9a078934d6dd47d3666ad903
Steven Duncan <steven.k.duncan@gmail.com>
2018-09-19 21:12:00 -0500

    Removed comment on fizzBuzz

--- src/recursion.js
+++ src/recursion.js
@@ -151,10 +151,6 @@ var buildList =(value, length) => {
 };
 
 // 19. Implement FizzBuzz. Given integer n, return an array of the string representations of 1 to n.
-// For multiples of three, output 'Fizz' instead of the number.
-// For multiples of five, output 'Buzz' instead of the number.
-// For numbers which are multiples of both three and five, output “FizzBuzz” instead of the number.
-// fizzBuzz(5) // ['1','2','Fizz','4','Buzz']
 var fizzBuzz = (n) => {
   return ((n % 3 === 0) && (n % 5 === 0)) ? fizzBuzz(n - 1).concat('FizzBuzz')
     : (n % 5 === 0) ? fizzBuzz(n - 1).concat('Buzz')
